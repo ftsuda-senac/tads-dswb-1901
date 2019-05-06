2f62924c18a50f5ec622bc9110ae5eff3ff920d8
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-05-06 19:19:44 -0300

    Exemplo Spring Security - projeto inicial

---
+++ exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/controller/HomeController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplospringsecurity.controller;
+
+import br.senac.tads.dsw.exemplospringsecurity.entidade.UsuarioSistema;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView mostrarHome(Authentication authentication) {
+        UsuarioSistema usuario = null;
+        if (authentication != null) {
+            usuario = (UsuarioSistema) authentication.getPrincipal();
+        }
+        return new ModelAndView("home").addObject("usuario", usuario);
+    }
+
+}
---
+++ exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/service/UsuarioSistemaService.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplospringsecurity.service;
+
+import br.senac.tads.dsw.exemplospringsecurity.entidade.Papel;
+import br.senac.tads.dsw.exemplospringsecurity.entidade.UsuarioSistema;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+public class UsuarioSistemaService {
+
+    private static final Map<String, UsuarioSistema> USUARIOS_CADASTRADOS
+            = new LinkedHashMap<>();
+
+    static {
+        USUARIOS_CADASTRADOS.put("denver", new UsuarioSistema("denver",
+                "Ricardo Ramos", "abcd1234",
+                Arrays.asList(new Papel("ROLE_PEAO"))));
+        USUARIOS_CADASTRADOS.put("tokio", new UsuarioSistema("tokio",
+                "Silene Oliveira", "abcd1234",
+                Arrays.asList(new Papel("ROLE_PEAO"))));
+        USUARIOS_CADASTRADOS.put("berlin", new UsuarioSistema("berlin",
+                "Andres de Fonollosa", "abcd1234",
+                Arrays.asList(new Papel("ROLE_PEAO"), new Papel("ROLE_FODON"))));
+        USUARIOS_CADASTRADOS.put("professor", new UsuarioSistema("professor",
+                "Sergio Marquina", "abcd1234",
+                Arrays.asList(new Papel("ROLE_PEAO"), new Papel("ROLE_GOD"))));
+    }
+
+    //@Override
+    public UsuarioSistema loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        if (USUARIOS_CADASTRADOS.containsKey(username)) {
+            return USUARIOS_CADASTRADOS.get(username);
+        }
+        throw new UsernameNotFoundException("Usuário não encontrado");
+    }
+
+}
